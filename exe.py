899db005cb5539b864604e64b3ecdc21082f8bf3
Lucas Albino <lucasalbino7@gmail.com>
2024-07-24 10:25:11 -0300

    model v1

---
+++ exe.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import streamlit as st 
+import joblib
+
+x_numericos = {'latitude': 0, 'longitude': 0, 'accommodates': 0, 'bathrooms': 0, 'bedrooms': 0, 'beds': 0, 'extra_people': 0,
+               'minimum_nights': 0, 'ano': 0, 'mes': 0, 'n_amenities': 0, 'host_listings_count': 0}
+
+x_tf = {'host_is_superhost': 0, 'instant_bookable': 0}
+
+x_listas = {'property_type': ['Apartment', 'Condominium', 'House', 'Outros'],
+            'room_type': ['Entire home/apt', 'Hotel room', 'Private room', 'Shared room'],
+            'cancelation_policy': ['flexible', 'moderate', 'strict', 'strict_14_with_grace_period']
+            }
+
+for item in x_numericos:
+    valor = st.number_input(f'{item}')
+
+for item in x_tf:
+    valor = st.selectbox(f'{item}', ('Sim', 'Não'))
+
+for item in x_listas:
+    valor = st.selectbox(f'{item}', x_listas[item])
+
+botao = st.button('Prever Valor do Imóvel')
---
+++ import pandas as pd.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df = pd.read_excel(r'C:\Users\User\Desktop\Pluralidades\Order.shipping.20221227_20230126.xlsx')
